8491bbca3e6ed7e2812a50f93795909de8283a78
Nicole Remy <nicoleremy95@gmail.com>
2020-11-10 15:54:10 -0800

    started pokecard.js component

---
+++ src/pokecard/Pokecard.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import App from '../App';
+
+class Pokecard extends Component {
+    render(){
+        return(
+            <div className="Pokecard"></div>
+        )
+    }
+}
+
+export default Pokecard;

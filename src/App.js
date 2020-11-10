f6ac487e81104f170724c11a73480eb4dce9b86a
Nicole Remy <nicoleremy95@gmail.com>
2020-11-10 15:57:14 -0800

    pokedex is now rendering pokecard component through the react app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React, {Component} from 'react';
+import Pokecard from './pokecard/Pokecard'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Pokecard/>
     </div>
   );
 }
--- src/pokecard/Pokecard.js
+++ src/pokecard/Pokecard.js
@@ -4,7 +4,9 @@ import App from '../App';
 class Pokecard extends Component {
     render(){
         return(
-            <div className="Pokecard"></div>
+            <div className="Pokecard">
+                <h2>POKECARD</h2>
+            </div>
         )
     }
 }

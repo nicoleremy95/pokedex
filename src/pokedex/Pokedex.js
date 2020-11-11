637d30db161f183433de387c8266b5ff3b8071cf
Nicole Remy <nicoleremy95@gmail.com>
2020-11-11 09:23:13 -0800

    removed static props from pokedex.js because they are coming from the parent pokegame.js

--- src/pokedex/Pokedex.js
+++ src/pokedex/Pokedex.js
@@ -3,18 +3,6 @@ import Pokecard from '../pokecard/Pokecard';
 import './Pokedex.css'
 
 class Pokedex extends Component{
-    static defaultProps = {
-        pokemon: [
-            {id: 4, name: "Charmander", type: "fire", exp: 62},
-            {id: 7, name: "Squirtle", type: "water", exp: 63},
-            {id: 11, name: "Metapod", type: "bug", exp: 3},
-            {id: 12, name: "Butterfree", type: "flying", exp: 15},
-            {id: 25, name: "Pikachu", type: "electric", exp: 83},
-            {id: 39, name: "Jigglypuff", type: "normal", exp: 23},
-            {id: 94, name: "Gengar", type: "poison", exp: 83},
-            {id: 133, name: "Eevee", type: "normal", exp: 19},
-        ]
-    }
     render(){
         let title;
         if(this.props.isWinner){

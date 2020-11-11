9f4bf0a408f19363c43c7d02570b8a6d647b5fda
Nicole Remy <nicoleremy95@gmail.com>
2020-11-10 16:15:59 -0800

    added in static defaultprops with an object containing the pokemons and their properties

--- src/pokedex/Pokedex.js
+++ src/pokedex/Pokedex.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react';
 import Pokecard from '../pokecard/Pokecard';
 
 class Pokedex extends Component{
+    static defaultProps = {
+        pokemon: [
+            {id: 4, name: "Charmander", type: "fire", exp: 62},
+            {id: 7, name: "Squirtle", type: "water", exp: 63},
+            {id: 11, name: "Metapod", type: "bug", exp: 3},
+            {id: 12, name: "Butterfree", type: "flying", exp: 15},
+            {id: 25, name: "Pikachu", type: "electric", exp: 83},
+            {id: 39, name: "Jigglypuff", type: "normal", exp: 23},
+            {id: 94, name: "Gengar", type: "poison", exp: 83},
+            {id: 133, name: "Eevee", type: "normal", exp: 19},
+        ]
+    }
     render(){
         return(
             <div className="Pokedex"></div>

5ee4c334a988c1725d8470d4d1bfb019070c23e7
Nicole Remy <nicoleremy95@gmail.com>
2020-11-10 16:41:43 -0800

    added more styling to the text on each card

--- src/pokecard/Pokecard.js
+++ src/pokecard/Pokecard.js
@@ -8,10 +8,10 @@ class Pokecard extends Component {
         let imgSrc = `${POKE_API}${this.props.id}.png`
         return(
             <div className="Pokecard">
-                    <h3>{this.props.name}</h3>
+                    <h3 className="Pokecard-title">{this.props.name}</h3>
                     <img src={imgSrc} alt={this.props.name}/>
-                    <h4>{this.props.type}</h4>
-                    <h4>{this.props.exp}</h4>
+                    <div className="Pokecard-data">{this.props.type}</div>
+                    <div className="Pokecard-data">{this.props.exp}</div>
             </div>
         )
     }

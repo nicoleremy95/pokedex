3f28ce3cd660db46dc94da546d72f26301c14bdd
Nicole Remy <nicoleremy95@gmail.com>
2020-11-10 16:10:59 -0800

    added pokedex component and folder with css

--- src/pokecard/Pokecard.js
+++ src/pokecard/Pokecard.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import App from '../App';
+import './Pokecard.css'
 
 const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
@@ -10,7 +10,9 @@ class Pokecard extends Component {
             <div className="Pokecard">
                 <h2>POKECARD</h2>
                     <h3>{this.props.name}</h3>
-                    <img src={imgSrc}/>
+                    <img src={imgSrc} alt={this.props.name}/>
+                    <h4>{this.props.type}</h4>
+                    <h4>{this.props.exp}</h4>
             </div>
         )
     }
---
+++ src/pokedex/Pokedex.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import Pokecard from '../pokecard/Pokecard';
+
+class Pokedex extends Component{
+    render(){
+        return(
+            <div className="Pokedex"></div>
+        )
+    }
+}
+
+export default Pokedex;

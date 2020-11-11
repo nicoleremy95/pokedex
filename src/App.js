cc06c0476e9dd6823da23f2e19223f17740496c8
Nicole Remy <nicoleremy95@gmail.com>
2020-11-10 16:29:09 -0800

    removed pokecard from app.js and added pokedex

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, {Component} from 'react';
-import Pokecard from './pokecard/Pokecard'
+import Pokedex from './pokedex/Pokedex'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Pokecard
-        id={4}
-        name="Charmander"
-        type="fire"
-        exp={62}
-      />
+      <Pokedex/>
     </div>
   );
 }
